a8baf39e7ac664340d94b27d9eeefa3707be1742
pokabu55 <okazumaru@gmail.com>
2022-06-17 18:27:29 +0900

    実行はできたけど

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <tcl/tcl.h>
 
-int main (int argc, char *argv[])
+#define BUFF_SIZE 1024
+
+int main()
 {
-    return 0;
+  char buff[BUFF_SIZE];
+  Tcl_Interp *ip = Tcl_CreateInterp();
+
+  printf("> ");
+  while (fgets(buff, BUFF_SIZE, stdin) != NULL) {
+    if (Tcl_Eval(ip, buff) != TCL_OK) {
+      fprintf(stderr, "%s\n", Tcl_GetStringResult(ip));
+    } else {
+      printf("%s\n", Tcl_GetStringResult(ip));
+    }
+    printf("> ");
+  }
+
+  Tcl_DeleteInterp(ip);
+  return 0;
 }
